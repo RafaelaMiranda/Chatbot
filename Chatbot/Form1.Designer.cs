cd4dadc25d7d1651de65570c5ebc31fa6976196d
Rafaela Gomes de Miranda <rafsbrug@gmail.com>
2018-07-15 19:19:08 -0300

    Começo dde projeto

---
+++ Chatbot/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Chatbot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pImagem = new System.Windows.Forms.Panel();
+            this.textUsuario = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBot = new System.Windows.Forms.TextBox();
+            this.btnMicrofone = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pImagem
+            // 
+            resources.ApplyResources(this.pImagem, "pImagem");
+            this.pImagem.Name = "pImagem";
+            // 
+            // textUsuario
+            // 
+            resources.ApplyResources(this.textUsuario, "textUsuario");
+            this.textUsuario.Name = "textUsuario";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // textBot
+            // 
+            resources.ApplyResources(this.textBot, "textBot");
+            this.textBot.Name = "textBot";
+            // 
+            // btnMicrofone
+            // 
+            resources.ApplyResources(this.btnMicrofone, "btnMicrofone");
+            this.btnMicrofone.Name = "btnMicrofone";
+            this.btnMicrofone.UseVisualStyleBackColor = true;
+            this.btnMicrofone.Click += new System.EventHandler(this.btnMicrofone_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnMicrofone);
+            this.Controls.Add(this.textBot);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textUsuario);
+            this.Controls.Add(this.pImagem);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pImagem;
+        private System.Windows.Forms.TextBox textUsuario;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBot;
+        private System.Windows.Forms.Button btnMicrofone;
+    }
+}
+
---
+++ Chatbot/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Speech.Recognition; // adicionar namespace
+
+namespace Chatbot
+{
+    public partial class Form1 : Form
+    {
+        private SpeechRecognitionEngine engine; // engine de Reconhecimento
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBot.Enabled = false;
+            textUsuario.Enabled = false;
+        }
+
+        private void LoadSpeech()
+        {
+            try
+            {
+                engine = new SpeechRecognitionEngine(); // instância
+                engine.SetInputToDefaultAudioDevice(); // microfone
+
+                string[] words = { "olá", "boa noite" , "Rafaela"};
+
+                // carregar a gramática
+                engine.LoadGrammar(new Grammar(new GrammarBuilder(new Choices(words))));
+
+                engine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(rec);
+
+                engine.RecognizeAsync(RecognizeMode.Multiple); // iniciar o reconhecimento
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Ocorreu no LoadSpeech(): " + ex.Message);
+            }
+        }
+
+        // metódo que é chamado quando algo é reconhecido
+        private void rec(object s, SpeechRecognizedEventArgs e)
+        {
+            pImagem.BackgroundImage = Image.FromFile(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal) + @"\TCC\Codigo\Chatbot\Chatbot\Eva\eve_eyes_02.jpg");
+            textUsuario.Text = e.Result.Text;
+        }
+
+        private void btnMicrofone_Click(object sender, EventArgs e)
+        {
+            LoadSpeech();
+        }
+    }
+}
